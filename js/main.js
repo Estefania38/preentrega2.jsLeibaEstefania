379f741678081024788eb0fcc1e33cc235dd34ed
Estefania38 <leibaestefania@gmail.com>
2023-03-08 19:16:48 -0300

    iniciando preyecto 3

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const carritoContent = document.getElementById("carritoContent"); 
+
+const verCarrito = document.getElementById("verCarrito"); 
+
+const modalContainer = document.getElementById("modal-container");
+
+const cantidadCarrito = document.getElementById("cantidadCarrito");
+
+
+
+let carrito = []
+
+disfraces.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "cards",
+        content.innerHTML = `
+    <img src="${product.img}"/>
+    <h3>${product.nombre}</h3>
+    <p class="precio"> $ ${product.precio}</p>    
+    `;
+    carritoContent.append(content);
+
+    let comprar = document.createElement('button');
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
+
+    content.append(comprar);
+
+    comprar.addEventListener("click", () => {
+
+    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+
+    if (repeat === true) {
+        carrito.map((prod) => {
+            if(prod.id === product.id) {
+                prod.cantidad++;
+            }
+        });
+    }  else{
+        carrito.push({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+            cantidad: product.cantidad,
+        });
+    }
+        console.log(carrito);
+        carritoCounter();
+    });
+});
+
